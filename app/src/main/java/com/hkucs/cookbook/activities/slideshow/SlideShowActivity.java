404a40d898d708d21e7596fb24fbc7eada2fdbd3
katenhy <>
2019-04-20 13:41:15 +0800

    change cat photo

--- app/src/main/java/com/hkucs/cookbook/activities/slideshow/SlideShowActivity.java
+++ app/src/main/java/com/hkucs/cookbook/activities/slideshow/SlideShowActivity.java
@@ -81,13 +81,12 @@ public class SlideShowActivity extends AppCompatActivity {
         GlobalVariable gv = new GlobalVariable();
 //        insertData();
 
-        if(gv.getNumOfRecipe()==0){
-            repId = intent.getIntExtra("recipeId",1);
-        }
-        else{
-            if (gv.getNumOfRecipe()==1){
+        if (gv.getNumOfRecipe() == 0) {
+            repId = intent.getIntExtra("recipeId", 1);
+        } else {
+            if (gv.getNumOfRecipe() == 1) {
                 repId = gv.getRecipeOne();
-            } else{
+            } else {
                 repId = gv.getRecipeOne();
                 repTwoId = gv.getRecipeTwo();
                 loadData(repTwoId);
@@ -151,7 +150,7 @@ public class SlideShowActivity extends AppCompatActivity {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber,String imgPath ,String description) {
+        public static PlaceholderFragment newInstance(int sectionNumber, String imgPath, String description) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -171,33 +170,34 @@ public class SlideShowActivity extends AppCompatActivity {
             descript.setText(getArguments().getString(ARG_DESCRIPTION));
             ImageView stepImg = (ImageView) rootView.findViewById(R.id.stepImg);
             String name = getArguments().getString(ARG_IMGPATH);
-            int img = getResources().getIdentifier(name,"drawable","com.hkucs.cookbook");
+            int img = getResources().getIdentifier(name, "drawable", "com.hkucs.cookbook");
             stepImg.setImageResource(img);
             TextView time = rootView.findViewById(R.id.timer);
             Button start = (Button) rootView.findViewById(R.id.start_button);
             String[] check = getArguments().getString(ARG_DESCRIPTION).split(":");
-            if(check[0].equals("Timer")) {
+            if (check[0].equals("Timer")) {
                 descript.setText("Countdown for: " + Integer.valueOf(check[1]) + " sec");
-                int t = Integer.valueOf(check[1]) *1000;
+                int t = Integer.valueOf(check[1]) * 1000;
                 CountDownTimer timerObj = new CountDownTimer(t, 1000) {
                     public void onTick(long millisUntilFinished) {
-                        int min = Math.round(millisUntilFinished/60000);
+                        int min = Math.round(millisUntilFinished / 60000);
                         String display = "";
-                        if (min > 10){
-                            if((millisUntilFinished / 1000)%60 >= 10) {
+                        if (min > 10) {
+                            if ((millisUntilFinished / 1000) % 60 >= 10) {
                                 display += "0" + min + ":" + (millisUntilFinished / 1000) % 60;
-                            }else{
+                            } else {
                                 display += "0" + min + ":0" + (millisUntilFinished / 1000) % 60;
                             }
-                        }else {
-                            if((millisUntilFinished / 1000)%60 >= 10) {
+                        } else {
+                            if ((millisUntilFinished / 1000) % 60 >= 10) {
                                 display += min + ":" + (millisUntilFinished / 1000) % 60;
-                            }else{
+                            } else {
                                 display += min + ":0" + (millisUntilFinished / 1000) % 60;
                             }
                         }
                         time.setText(display);
                     }
+
                     public void onFinish() {
                         time.setText("Done!");
                         Vibrator vb = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
@@ -209,7 +209,7 @@ public class SlideShowActivity extends AppCompatActivity {
                         timerObj.start();
                     }
                 });
-            }else{
+            } else {
                 time.setVisibility(View.GONE);
                 start.setVisibility(View.GONE);
             }
@@ -232,7 +232,7 @@ public class SlideShowActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1, pArrayList.get(position).getImgPath(),pArrayList.get(position).getDescription());
+            return PlaceholderFragment.newInstance(position + 1, pArrayList.get(position).getImgPath(), pArrayList.get(position).getDescription());
         }
 
         @Override
@@ -245,18 +245,19 @@ public class SlideShowActivity extends AppCompatActivity {
     /*
      * read jsonfile and insert them to the database
      * */
-    private JSONArray readJson(){
+    private JSONArray readJson() {
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset(this));
             Log.d("hi", "readJson:OK");
             return obj.getJSONArray("procedure");
-        }catch (JSONException e){
+        } catch (JSONException e) {
             Log.d(TAG, "readJson: ERROR cannot read json string as object");
             e.printStackTrace();
         }
         return null;
     }
-    private String loadJSONFromAsset(Context context){
+
+    private String loadJSONFromAsset(Context context) {
         String json = null;
         try {
             InputStream is = context.getAssets().open(JSON_FILE);
@@ -279,42 +280,43 @@ public class SlideShowActivity extends AppCompatActivity {
         return json;
 
     }
-    private ArrayList<Procedure> getDataFromJson (){
+
+    private ArrayList<Procedure> getDataFromJson() {
         ArrayList<Procedure> proceduresList = new ArrayList<Procedure>();
         JSONArray arr = readJson();
-        try{
-            for(int i=0; i<arr.length(); i++){
+        try {
+            for (int i = 0; i < arr.length(); i++) {
                 JSONObject obj = arr.getJSONObject(i);
                 Integer id = Integer.valueOf(obj.getString("recipeId"));
                 Integer step = Integer.valueOf(obj.getString("step"));
                 String imgName = obj.getString("image_path");
                 String description = obj.getString("description");
-                Procedure item = new Procedure(id,step,imgName,description);
+                Procedure item = new Procedure(id, step, imgName, description);
                 Log.d(TAG, description);
                 proceduresList.add(item);
             }
-        }catch (JSONException e){
+        } catch (JSONException e) {
             Log.d("hi", "getDataFromJson: ERROR in getting json object ");
             e.printStackTrace();
         }
         return proceduresList;
     }
 
-    private void insertData(){
+    private void insertData() {
 //       get json and read json
         ArrayList<Procedure> proceduresList = getDataFromJson();
         databaseHelper.cleanTable2();
         boolean result = databaseHelper.insertMultipleProcedure(proceduresList);
-        if(result){
-            Toast.makeText(this,"Sucessfully insert data",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this,"Error in insert data",Toast.LENGTH_SHORT).show();
+        if (result) {
+            Toast.makeText(this, "Successfully insert data", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Error in insert data", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    private void loadData(Integer rep_Id){
-        Log.d("RecipeMenu","load data");
+    private void loadData(Integer rep_Id) {
+        Log.d("RecipeMenu", "load data");
         ArrayList<Procedure> tempArrayList = databaseHelper.getProcedureById(rep_Id);
         pArrayList.addAll(tempArrayList);
         Collections.sort(pArrayList, new Comparator<Procedure>() {
@@ -323,7 +325,7 @@ public class SlideShowActivity extends AppCompatActivity {
                 return o1.getStep() - o2.getStep();
             }
         });
-        Log.d("RecipeMenu","recipeItemArrayList: "+pArrayList);
+        Log.d("RecipeMenu", "recipeItemArrayList: " + pArrayList);
     }
 
 }
